b77facb6c1305e9a157e434b3facbdb77c2c7c9c
Sithu Aung Khant <sithu.sithuthedev@gmail.com>
2023-08-24 14:33:24 +0630

    added index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card-1");
+const card_2 = document.querySelector(".card-2");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    card_1.classList.add("hide");
+    card_2.classList.remove("hide");
+})
